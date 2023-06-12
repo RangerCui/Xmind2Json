26a601fcbd11b9f833f9ce6623f2563c397ad101
hubery.cui <ranger_cui@163.com>
2023-06-12 11:25:26 +0800

    init gem

---
+++ lib/Xmind2Json.rb
@@ -0,0 +1,13 @@
+require "Xmind2Json/version"
+
+module Xmind2Json
+
+  class Error < StandardError; end
+  # Your code goes here...
+
+  class Xmind2Json
+    def test(content)
+      p content + 'asjdlfjalsdfjlks'
+    end
+  end
+end
